f2f8ce4fe3a08ea0974a22bdb49ef3caa6950d3d
Robinnicholas <robinnicholas2000@gmail.com>
2021-10-07 23:45:07 +0530

    navbar changed and corrections updated

--- js/main.js
+++ js/main.js
@@ -1,38 +1,21 @@
 function customInputchange(){
-    let placeholderTexts = document.querySelectorAll(".placeholder-text");
     let customInputs = document.querySelectorAll(".custom-input");
+    let placeHolderTexts = document.querySelectorAll('.placeholder-text');
 
-    customInputs[0].addEventListener('focusin', () => {
-        placeholderTexts[0].style.transform = "translate(25px, -15px)";
-        placeholderTexts[0].style.transition = " .2s ease-in-out";
-        customInputs[0].parentElement.previousElementSibling.checked = true;
-    });
-    
-    customInputs[0].addEventListener('focusout', () => {
-        if(!customInputs[0].value){
-            placeholderTexts[0].style.transform = "translate(45px, 20px)";
-            placeholderTexts[0].style.transition = " .2s ease-in-out";
-        }else{
-            placeholderTexts[0].style.transform = "translate(25px, -15px)";
-            placeholderTexts[0].style.transition = " .2s ease-in-out";
-        }
-        
-    });
-    customInputs[1].addEventListener('focusin', () => {
-        placeholderTexts[1].style.transform = "translate(25px, -15px)";
-        placeholderTexts[1].style.transition = " .2s ease-in-out";
-        customInputs[1].parentElement.previousElementSibling.checked = true;
-    });
-    
-    customInputs[1].addEventListener('focusout', () => {
-        if(!customInputs[1].value){
-            placeholderTexts[1].style.transform = "translate(45px, 20px)";
-            placeholderTexts[1].style.transition = " .2s ease-in-out";
-        }else{
-            placeholderTexts[1].style.transform = "translate(25px, -15px)";
-            placeholderTexts[1].style.transition = " .2s ease-in-out";
-        }
-    });
+    for(let i = 0; i< customInputs.length ; i++){
+        customInputs[i].addEventListener("focusin", () => {
+            if(!customInputs[i].value){
+                customInputs[i].parentElement.previousElementSibling.checked = "true";
+                
+            }
+        })
+        customInputs[i].addEventListener("focusout", () => {
+            if(customInputs[i].value){
+                console.log("value is present");
+                placeHolderTexts[i].style.transform = "translate(-20px,-33px)";
+            }
+        })
+    }
 }
 
 function navigation(){
