fa8f8c84cce911a468ae4a0ffdf66d40b3e4f1f7
Robinnicholas <robinnicholas2000@gmail.com>
2021-10-12 23:12:48 +0530

    recreated

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
 function navigation(){
-    let nav = document.querySelector('nav');
+    let nav = document.querySelector('header ul');
     nav.classList.toggle("show");
-    let line1 = document.querySelector('.line1');
-    let line2 = document.querySelector('.line2');
-    let line3 = document.querySelector('.line3');
-
-    let show = document.querySelector(".show");
-    if(show){
-        nav.style.transform = "translate(0)";
-        line1.style.display = "none";
-        line2.style.transform = "rotate(45deg)";
-        line2.style.transition = ".5s";
-        line3.style.transform = "rotate(315deg) translate(5px,-6px)";
-        line3.style.transition = ".5s";
-    }else{
-        nav.style.transform = "translate(100%)";
-        line1.style.display = "block";
-        line2.style.transform = "rotate(0deg)";
-        line2.style.transition = ".5s";
-        line3.style.transform = "rotate(0deg)";
-        line3.style.transition = ".5s";
-    }
 }
 
 function customInputchange(){
     let customInputs = document.querySelectorAll(".custom-input");
-    let placeHolderTexts = document.querySelectorAll('.placeholder-text');
-    let radioCircles = document.querySelectorAll('label');
-
-    for(let i = 0; i< customInputs.length ; i++){
-        radioCircles[i].addEventListener("click", () => {
-            console.log("label clicked");
-            customInputs[i].parentElement.previousElementSibling.checked = "true";
+    for(let i = 0; i < customInputs.length ; i++){
+        customInputs[i].addEventListener("click", () => {
+            customInputs[i].parentElement.previousElementSibling.checked = true;
         })
-        customInputs[i].addEventListener("focusin", () => {
-            customInputs[i].parentElement.previousElementSibling.checked = "true";
-            customInputs[i].addEventListener('input', () => {
-                placeHolderTexts[i].style.transform = "translate(0,-25px)";
-                if(!customInputs[i].value){
-                    placeHolderTexts[i].style.transform = "translate(0)";
-                }
-            });
-        });
+        
+        customInputs[i].addEventListener("change", () => {
+            if(customInputs[i].value){
+                console.log("value present");
+                customInputs[i].value = '';
+            }
+        })
     }
+}
+
+function headerScroll(){
+    window.addEventListener('scroll', () => {
+        let header = document.querySelector("header");
+        let heroContent = document.querySelector(".hero-content");
+        if( header.clientHeight > heroContent.getBoundingClientRect().top){
+            header.classList.add("add-bg");
+        }
+        if(document.body.getBoundingClientRect().y == 0){
+            header.classList.remove("add-bg");
+        }
+    })
 }
 
 window.addEventListener('load', () => {
     customInputchange();    
+    headerScroll();
 })
